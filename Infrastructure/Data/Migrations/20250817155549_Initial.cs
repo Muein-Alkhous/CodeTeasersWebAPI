273a07ebb8360506b5e76eef1850765cc0b73efd
Muein-Alkhous <alkhousmueen3@gmail.com>
2025-08-17 18:58:36 +0300

    new migration

---
+++ Infrastructure/Data/Migrations/20250817155549_Initial.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:difficulty", "Easy,Medium,Hard,Expert")
+                .Annotation("Npgsql:Enum:rank", "Newbie,Beginner,Apprentice,Coder,Pro,Expert,Master");
+
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("categories_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "problems",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    title = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    difficulty = table.Column<string>(type: "difficulty", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("problems_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    username = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false, comment: "Hashed password"),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("user_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "descriptions",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    description_path = table.Column<string>(type: "text", nullable: false),
+                    short_description = table.Column<string>(type: "text", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("descriptions_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "descriptions_id_fkey",
+                        column: x => x.id,
+                        principalTable: "problems",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "problem_categories",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    category_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    problem_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("problem_categories_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_problem_categories_category_id",
+                        column: x => x.category_id,
+                        principalTable: "categories",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_problem_categories_problem_id",
+                        column: x => x.problem_id,
+                        principalTable: "problems",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "templates",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    problem_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    template_path = table.Column<string>(type: "text", nullable: false),
+                    language = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false, defaultValueSql: "'python'::character varying"),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("templates_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "templates_problem_id_fkey",
+                        column: x => x.problem_id,
+                        principalTable: "problems",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tests",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    problem_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    language = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false, defaultValueSql: "'python'::character varying"),
+                    test_path = table.Column<string>(type: "text", nullable: false),
+                    num_cases = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("tests_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_tests_problem",
+                        column: x => x.problem_id,
+                        principalTable: "problems",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "submissions",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    problem_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    language = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false, defaultValueSql: "'python'::character varying"),
+                    points = table.Column<int>(type: "integer", nullable: false),
+                    passed_tests = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("submissions_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_submissions_problem_id",
+                        column: x => x.problem_id,
+                        principalTable: "problems",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_submissions_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_statuses",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    points = table.Column<int>(type: "integer", nullable: false, defaultValue: 0),
+                    rank = table.Column<string>(type: "rank", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("user_statuses_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_user_statuses_user",
+                        column: x => x.id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "uq_categories_title",
+                table: "categories",
+                column: "title",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_problem_categories_category_id",
+                table: "problem_categories",
+                column: "category_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_problem_categories_problem_id",
+                table: "problem_categories",
+                column: "problem_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_submissions_user_id",
+                table: "submissions",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "uq_submissions_language_problem_id_user_id",
+                table: "submissions",
+                columns: new[] { "problem_id", "user_id", "language" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "uq_templates_problem_id_language",
+                table: "templates",
+                columns: new[] { "problem_id", "language" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tests_problem_id",
+                table: "tests",
+                column: "problem_id");
+
+            migrationBuilder.CreateIndex(
+                name: "uq_users_email",
+                table: "user",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "uq_users_username",
+                table: "user",
+                column: "username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "descriptions");
+
+            migrationBuilder.DropTable(
+                name: "problem_categories");
+
+            migrationBuilder.DropTable(
+                name: "submissions");
+
+            migrationBuilder.DropTable(
+                name: "templates");
+
+            migrationBuilder.DropTable(
+                name: "tests");
+
+            migrationBuilder.DropTable(
+                name: "user_statuses");
+
+            migrationBuilder.DropTable(
+                name: "categories");
+
+            migrationBuilder.DropTable(
+                name: "problems");
+
+            migrationBuilder.DropTable(
+                name: "user");
+        }
+    }
+}
