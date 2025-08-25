b1609664f019624a66342370dd58694f8b81a9f0
Muein-Alkhous <alkhousmueen3@gmail.com>
2025-08-25 13:21:38 +0300

    Add migration

---
+++ Infrastructure/Data/Migrations/20250825083654_RemoveFilePathFromDescription.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveFilePathFromDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "description_path",
+                table: "descriptions");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "file_data",
+                table: "descriptions",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "file_data",
+                table: "descriptions");
+
+            migrationBuilder.AddColumn<string>(
+                name: "description_path",
+                table: "descriptions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
